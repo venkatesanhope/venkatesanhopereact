eac98a4e2f3595eed3683e7587d4098a340720f3
venkatesanhope <venkatesan.hope@gmail.com>
2023-04-12 18:22:24 +0530

    chan

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import Footer from './component/Footer';
 import Header from './component/Header';
 import Dashboard from './component/Dashboard';
+import Tasktwo from './component/Tasktwo'
 function App() {
   return (
     <div className="App">
         <Header></Header>
         <Dashboard></Dashboard>
+        <Tasktwo></Tasktwo>
         <Footer></Footer>
     </div>
   );
---
+++ src/component/Tasktwo.tsx
@@ -0,0 +1,68 @@
+import ReactDOM from 'react-dom/client';
+import { useState,useCallback,useMemo } from "react";
+
+
+const Tasktwo = () =>
+{
+
+    const divStyle = {
+        width: "15px",
+        height: "15px",
+        background: "red"
+      };
+
+
+ const starString:any = [];
+ const [rownumber,rowfunc] = useState(0)
+ const [colnumber,colfunc] = useState(0)   
+
+const incrementRow= useCallback(() => {
+    colfunc(0);
+    rowfunc(rownumber + 1);
+  }, [rownumber]);
+
+  const incrementCol= useCallback(() => {
+   
+   colfunc(colnumber + 1);
+    
+  }, [colnumber]);
+
+
+
+  const stars = useMemo(() => {
+   
+    for (let index = 0; index < rownumber; index++) {
+
+        starString.push()
+
+      for (let index = 0; index < colnumber; index++) {
+        
+        starString.push("*")
+      }
+     
+      starString.push(<br></br>)
+    }
+    return starString;
+  }, [rownumber,colnumber]);
+
+
+  
+return(
+    <>
+    <hr></hr>
+    <h1> Task Two</h1>
+   
+    {rownumber}
+    {colnumber}
+  
+    <button onClick={incrementRow}>Increment Row</button>
+    <button onClick={incrementCol}>Increment Col</button>
+   <br></br>
+    {stars}
+   
+    </>
+)
+
+}
+
+export default Tasktwo
