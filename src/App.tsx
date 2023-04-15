8adbe9380c37e40f7af83e7e6f6284d5bcc4fdad
venkatesanhope <venkatesan.hope@gmail.com>
2023-04-15 18:31:10 +0530

    info

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import Header from './component/Header';
 import Dashboard from './component/Dashboard';
 import Tasktwo from './component/Tasktwo';
 import Taskrcol from './component/Taskrcol';
-
+import Studentinfo from './component/Studentinfo';
 function App() {
   return (
     <div className="App">
         <Header></Header>
         <Dashboard></Dashboard>
-        <Tasktwo></Tasktwo>
-        <Taskrcol></Taskrcol>
+        <Studentinfo></Studentinfo>
         <Footer></Footer>
     </div>
   );
 }
 
 export default App;
+// <Tasktwo></Tasktwo>
+//<Taskrcol></Taskrcol>
